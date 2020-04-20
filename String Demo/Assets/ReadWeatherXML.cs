33c0d9545e732875d31efd075fafcef1c1b0a408
Jordin De La Rosa <jordindlrosa@gmail.com>
2020-04-19 20:56:06 -0400

    Manual Bill Pay Try #1

---
+++ String Demo/Assets/ReadWeatherXML.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class ReadWeatherXML : MonoBehaviour
+{
+    string key = "5ce4711625e2362ce6528583bed12e19";
+    string apiReturn = "";
+    public string cityName = "";
+    public string windDirection = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetWeather());
+        
+    }
+    IEnumerator GetWeather()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://api.openweathermap.org/data/2.5/weather?zip=10027&mode=xml&APPID=" + key);
+            yield return www.SendWebRequest();
+
+        if(!www.isNetworkError && !www.isHttpError)
+        {
+            //Get text content like this:
+            Debug.Log(www.downloadHandler.text);
+            apiReturn = www.downloadHandler.text;
+        }
+        else
+        {
+            Debug.Log(www.error + " " + www);
+        }
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        if(apiReturn.Length != 0)
+        {
+            int tagForNameStart = apiReturn.IndexOf("name="+ 2); //This gets us the index of the n for the first time it finds "name="
+            int tagForCityStart = tagForNameStart + 6;
+            int tagForNameEnd = apiReturn.IndexOf("coord");
+            int tagForCityEnd = tagForNameEnd - 3;
+
+            cityName = apiReturn.Substring(tagForCityStart, tagForNameEnd - tagForCityStart);
+
+
+            string everythingAfterWind = apiReturn.Substring(apiReturn.IndexOf("win"));
+            string everythingAfterDirection = everythingAfterWind.Substring(apiReturn.IndexOf("direction"));
+            int tagForNameStart = everythingAfterDirection.IndexOf("name=");
+
+           
+        }
+    }
+}
---
+++ String Demo/Assets/String.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class String : MonoBehaviour
+{
+    public char myChar;
+    public string myString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(myChar);
+        //Debug.Log(myString);
+        //Debug.Log(myChar + " " + myString);
+        //Debug.Log(myString.Substring(3));
+        //Debug.Log(myString.Substring(3, 1));
+        //Debug.Log(myString);
+        //Debug.Log(myString.ToUpper());
+        //Debug.Log(myString.ToLower());
+        string lowerCaseVersionOfMyString = myString.ToLower();
+
+        if (lowerCaseVersionOfMyString.IndexOf(" is ") != -1)
+
+            Debug.Log(myString.IndexOf(" is "));
+        else if (lowerCaseVersionOfMyString.IndexOf(" is, ") != -1)
+            Debug.Log(lowerCaseVersionOfMyString.IndexOf(" is, "));
+        else if (lowerCaseVersionOfMyString.IndexOf(" is. ") != -1)
+            Debug.Log(lowerCaseVersionOfMyString.IndexOf(" is. "));
+       
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
